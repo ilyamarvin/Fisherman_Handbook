c539d36651fab01219ceafb96f953dd65ea7a588
ilyamarvin <ilya.sereda2002@mail.ru>
2020-04-22 16:09:21 +0300

    Updating functional of our app.

--- app/src/main/java/com/ilyamarvin/fishermanhandbook/MainActivity.java
+++ app/src/main/java/com/ilyamarvin/fishermanhandbook/MainActivity.java
@@ -1,9 +1,13 @@
 package com.ilyamarvin.fishermanhandbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -20,14 +24,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar toolbar;
     private DrawerLayout drawer;
+    private ListView list;
+    private String[] array;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        list = findViewById(R.id.listView);
+        array = getResources().getStringArray(R.array.starter_array);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>(Arrays.asList(array)));
+        list.setAdapter(adapter);
+
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
@@ -36,28 +53,96 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toogle);
         toogle.syncState();
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        navigationView.setNavigationItemSelectedListener(this);
 
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, text_content_activity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        toolbar.setTitle(R.string.menu_starter);
         return true;
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if(id == R.id.nav_fish) {
-
+        if(id == R.id.nav_starter) {
+            toolbar.setTitle(R.string.menu_starter);
+            array = getResources().getStringArray(R.array.starter_array);
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
         }
-        else if (id == R.id.nav_bait) {
-
+        else if (id == R.id.nav_fish) {
+            toolbar.setTitle(R.string.menu_fish);
+            array = getResources().getStringArray(R.array.fish_array);
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
         }
+        else if (id == R.id.nav_hooks) {
+            toolbar.setTitle(R.string.menu_hooks);
+            array = getResources().getStringArray(R.array.hooks_array);
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+        else if (id == R.id.nav_artbait) {
+            toolbar.setTitle(R.string.menu_artbait);
+            array = getResources().getStringArray(R.array.artbait_array);
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+        else if (id == R.id.nav_rig) {
+            toolbar.setTitle(R.string.menu_rig);
+            array = getResources().getStringArray(R.array.rig_array);
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+        else if (id == R.id.nav_lure) {
+            toolbar.setTitle(R.string.menu_lure);
+            array = getResources().getStringArray(R.array.lure_array);
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+        else if (id == R.id.nav_tips) {
+            toolbar.setTitle(R.string.menu_tips);
+            array = getResources().getStringArray(R.array.tips_array);
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+        else if (id == R.id.nav_weather) {
+            toolbar.setTitle(R.string.menu_weather);
+            array = getResources().getStringArray(R.array.weather_array);
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
