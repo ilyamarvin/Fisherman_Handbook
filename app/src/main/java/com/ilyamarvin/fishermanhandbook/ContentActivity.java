6e2af4177f8020c7ecbdaeeba677f13b21800fae
ilyamarvin <ilya.sereda2002@mail.ru>
2020-05-08 11:57:02 +0300

    Complete the fish information, working with hooks info.

--- app/src/main/java/com/ilyamarvin/fishermanhandbook/ContentActivity.java
+++ app/src/main/java/com/ilyamarvin/fishermanhandbook/ContentActivity.java
@@ -18,20 +18,36 @@ public class ContentActivity extends AppCompatActivity {
     private int category = 0;
     private int position = 0;
     private ImageView imContent;
-    private int[] array_starter = {R.string.starter_1, R.string.starter_2, R.string.starter_3, R.string.starter_4, R.string.starter_5,
-            R.string.starter_6, R.string.starter_7};
+    private int[] array_starter = {
+            R.string.starter_1, R.string.starter_2, R.string.starter_3, R.string.starter_4, R.string.starter_5,
+            R.string.starter_6, R.string.starter_7
+    };
     private int[] array_fish = {
             R.string.fish_1, R.string.fish_2, R.string.fish_3, R.string.fish_4, R.string.fish_5,
             R.string.fish_6, R.string.fish_7, R.string.fish_8, R.string.fish_9, R.string.fish_10, R.string.fish_11,
             R.string.fish_12, R.string.fish_13, R.string.fish_14, R.string.fish_15, R.string.fish_16
     };
-    private int[] array_image_fish = {R.drawable.ukleyka};
-    private int[] array_title_fish = {R.string.fish_name_1, R.string.fish_name_2, R.string.fish_name_3, R.string.fish_name_4, R.string.fish_name_5,
+    private int[] array_image_fish = {
+            R.drawable.ukleyka, R.drawable.plotva, R.drawable.elec, R.drawable.krasnoperka, R.drawable.karas, R.drawable.karp,
+            R.drawable.schuka, R.drawable.lesh, R.drawable.som, R.drawable.rotan, R.drawable.okun, R.drawable.golavl, R.drawable.zhereh, R.drawable.yaz,
+            R.drawable.gustera, R.drawable.amur
+    };
+    private int[] array_title_fish = {
+            R.string.fish_name_1, R.string.fish_name_2, R.string.fish_name_3, R.string.fish_name_4, R.string.fish_name_5,
             R.string.fish_name_6, R.string.fish_name_7, R.string.fish_name_8, R.string.fish_name_9, R.string.fish_name_10, R.string.fish_name_11,
-            R.string.fish_name_12, R.string.fish_name_13, R.string.fish_name_14, R.string.fish_name_15, R.string.fish_name_16, R.string.fish_name_17};
+            R.string.fish_name_12, R.string.fish_name_13, R.string.fish_name_14, R.string.fish_name_15, R.string.fish_name_16, R.string.fish_name_17
+    };
     private int[] array_hooks = {
             R.string.hook_1, R.string.hook_2, R.string.hook_3, R.string.hook_4,
             R.string.hook_5, R.string.hook_6, R.string.hook_7
+    };
+    private int[] array_image_hook = {
+            R.drawable.hooksizestandart, R.drawable.hooksizekarp, R.drawable.hooksizeofset, R.drawable.hooksizetroynik,
+            R.drawable.hooksizejig, R.drawable.hooksizeogruz, R.drawable.hookjivec
+    };
+    private int[] array_title_hook = {
+            R.string.hook_name_1, R.string.hook_name_2, R.string.hook_name_3, R.string.hook_name_4,
+            R.string.hook_name_5, R.string.hook_name_6, R.string.hook_name_7
     };
     private int[] array_artbait = {
             R.string.artbait_1, R.string.artbait_2, R.string.artbait_3, R.string.artbait_4,
@@ -90,12 +106,14 @@ public class ContentActivity extends AppCompatActivity {
                 text_content.setText(array_starter[position]);
                 break;
             case 1:
-             //   imContent.setImageResource(array_image_fish[position]);
+                imContent.setImageResource(array_image_fish[position]);
                 text_content.setText(array_fish[position]);
                 actionBar.setTitle(array_title_fish[position]);
                 break;
             case 2:
+                imContent.setImageResource(array_image_hook[position]);
                 text_content.setText(array_hooks[position]);
+                actionBar.setTitle(array_title_hook[position]);
                 break;
             case 3:
                 text_content.setText(array_artbait[position]);
